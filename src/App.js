e5dca4db3ff0fad6af179f00aa10ec3398cd1f54
Maxence Califano <maxence.califano@gmail.com>
2021-06-12 10:51:56 +0200

    12062021

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import { connect } from "react-redux";
 import Button from "./Components/Button.js";
 import Controls from "./Components/Controls";
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-//import { faPlay, faPause, faUndoAlt } from "@fortawesome/free-solid-svg-icons";
 import "./index.css";
 let countDown;
 
@@ -124,16 +122,24 @@ class App extends React.Component {
               id="start_stop"
               timerControl={this.startStop}
               controlName={
-                <div></div>
-              
-                 /* "play/pause" <FontAwesomeIcon icon={faPlay} />
-                  <FontAwesomeIcon icon={faPause} /> */
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff">
+                    <path
+                        d="M0 0h24v24H0V0z" fill="none"/>
+                    <path d="M10 8.64L15.27 12 10 15.36V8.64M8 5v14l11-7L8 5z"
+                    />
+                  </svg>
+                  <svg style ={{marginLeft: -10}}xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff">
+                    <path d="M0 0h24v24H0V0z" fill="none"/>
+                    <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
+                  </svg>
+                </div>
               }
             />
             <Controls
               id="reset"
               timerControl={this.reset}
-              controlName={"reset"}
+              controlName={<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><g><rect fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/></g><g><g/><path d="M12,5V1L7,6l5,5V7c3.31,0,6,2.69,6,6s-2.69,6-6,6s-6-2.69-6-6H4c0,4.42,3.58,8,8,8s8-3.58,8-8S16.42,5,12,5z"/></g></svg>}
               /*<FontAwesomeIcon
                 icon={faUndoAlt}
                 style={{ fontSize: "large" }}
